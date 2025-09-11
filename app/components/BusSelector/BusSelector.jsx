f52d2a357c92df30caf49de3bf1e9b6bd05cbe3f
Osid ALkourd <alkordosid73@gmail.com>
2025-09-11 10:24:10 +0300

    display all buses as buttons, display bus routes on the map

--- app/components/BusSelector/BusSelector.jsx
+++ app/components/BusSelector/BusSelector.jsx
@@ -1,12 +1,22 @@
+"use client";
+
+import busesData from "../../data/buses.json";
 import "./BusSelector.css";
 
-function BusSelector() {
+function BusSelector({ selectedBusId, onSelectBus }) {
+  const busLines = busesData.bus_lines;
+
   return (
     <div className="bus-selector">
-      <button className="active">Bus 1</button>
-      <button>Bus 2</button>
-      <button>Bus 3</button>
-      <button>Bus 4</button>
+      {busLines.map((bus) => (
+        <button
+          key={bus.id}
+          className={selectedBusId === bus.id ? "active" : ""}
+          onClick={() => onSelectBus(bus.id)}
+        >
+          Bus {bus.id}
+        </button>
+      ))}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Navbar from "./components/Navbar/Navbar";
 import HeaderSection from "./components/HeaderSection/HeaderSection";
 import SectionTitle from "./components/SectionTitle/SectionTitle";
@@ -5,26 +6,29 @@ import BusSelector from "./components/BusSelector/BusSelector";
 import BusMap from "./components/BusMap/BusMap";
 import BusSchedule from "./components/BusSchedule/BusSchedule";
 import Footer from "./components/Footer/Footer";
+import { useState } from "react";
+
 
 export default function Home() {
+  const [selectedBusId, setSelectedBusId] = useState<number | null>(null);
+
   return (
     <div className="App">
       {/* شريط التنقل */}
       <Navbar />
-
       {/* العنوان الرئيسي */}
       <HeaderSection />
-
       {/* خريطة الباص */}
       <SectionTitle title="Active Bus Map" />
-      <BusSelector />
-      <BusMap />
-
+      <BusSelector
+        selectedBusId={selectedBusId}
+        onSelectBus={setSelectedBusId}
+      />{" "}
+      <BusMap selectedBusId={selectedBusId} />
       {/* جدول مواعيد الباص */}
       <SectionTitle title="Bus Schedule" />
       <BusSelector />
       <BusSchedule />
-
       {/* الفوتر */}
       <Footer />
     </div>
