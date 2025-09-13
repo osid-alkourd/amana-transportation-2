7cd2f673f9a43012b79048bdca2fcd5a1615664d
Osid ALkourd <alkordosid73@gmail.com>
2025-09-13 17:29:50 +0300

    update bus schedule and update map color

--- app/components/BusMap/BusMap.jsx
+++ app/components/BusMap/BusMap.jsx
@@ -36,8 +36,8 @@ export default function BusMap({ selectedBusId }) {
           style={{ height: "100%", width: "100%" }}
         >
           <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           />
 
           {selectedBus && (
